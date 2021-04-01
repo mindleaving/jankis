06bc71a856a5b6388c0aba0424d6125025e6587d
Jan Scholtyssek <jan@janscholtyssek.dk>
2021-04-01 21:59:37 +0200

    Scanning code for localization strings. Generating localization dictionaries.

--- JanKIS.Tools/LocalizationStringSearcher.cs
+++ JanKIS.Tools/LocalizationStringSearcher.cs
@@ -1,12 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
 
 namespace JanKIS.Tools
 {
-    class LocalizationStringSearcher
+    public class LocalizationStringSearcher
     {
+        private const string FrontendDirectory = @"G:\Projects\JanKIS\jankis-frontend";
+
+        [Test]
+        public void CreateLocalizationIdDictionary()
+        {
+            var localizationOutputFile = Path.Combine(FrontendDirectory, "src/resources", "translation.en.json");
+            var tsFiles = Directory.GetFiles(Path.Combine(FrontendDirectory, "src"), "*.ts?", SearchOption.AllDirectories);
+            var existingLocalizations = JObject.Parse(File.ReadAllText(localizationOutputFile));
+            var resourceIds = new List<string>(existingLocalizations.Properties().Select(x => x.Name));
+            foreach (var tsFile in tsFiles)
+            {
+                var fileContent = File.ReadAllLines(tsFile);
+                foreach (var line in fileContent)
+                {
+                    var matches = Regex.Matches(line, "resolveText\\([\"'](?<ResourceID>[^\"]+)[\"']\\)");
+                    foreach (Match match in matches)
+                    {
+                        if(!match.Groups["ResourceID"].Success)
+                            continue;
+                        var resourceId = match.Groups["ResourceID"].Value;
+                        resourceIds.Add(resourceId);
+                    }
+                }
+            }
+
+            
+            var resourceDictionary = new JObject();
+            foreach (var resourceId in resourceIds.Distinct().OrderBy(x => x))
+            {
+                var existingValue = existingLocalizations[resourceId]?.Value<string>();
+                resourceDictionary[resourceId] = existingValue ?? "";
+                Console.WriteLine($"{resourceId}: {existingValue}");
+            }
+
+            File.WriteAllText(localizationOutputFile, JsonConvert.SerializeObject(resourceDictionary, Formatting.Indented));
+        }
+
+        [Test]
+        [TestCase("de")]
+        [TestCase("dk")]
+        public void CreateLocalizationsIdsInSecondaryDictionaries(string language)
+        {
+            var primaryLanguage = "en";
+            var primaryDictionaryFile = Path.Combine(
+                FrontendDirectory,
+                "src/resources",
+                $"translation.{primaryLanguage}.json");
+            var secondaryDictionaryFile = Path.Combine(
+                FrontendDirectory,
+                "src/resources",
+                $"translation.{language}.json");
+            var primaryJObject = JObject.Parse(File.ReadAllText(primaryDictionaryFile));
+            var secondaryJObject = JObject.Parse(File.ReadAllText(secondaryDictionaryFile));
+            foreach (var resourceId in primaryJObject.Properties().Select(x => x.Name))
+            {
+                if(secondaryJObject.ContainsKey(resourceId))
+                    continue;
+                secondaryJObject[resourceId] = "";
+            }
+            File.WriteAllText(secondaryDictionaryFile, JsonConvert.SerializeObject(secondaryJObject, Formatting.Indented));
+
+
+        }
     }
 }
